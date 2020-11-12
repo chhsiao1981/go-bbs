52c93eb3c66a3e70e0761972c4bf0047cb363c32
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2020-11-12 12:05:16 -0500

    fixed-bytes

--- ptttype/utils.go
+++ ptttype/utils.go
@@ -25,37 +25,3 @@ func SetBBSHOME(bbshome string) string {
 
 	return origBBSHome
 }
-
-//FixedBytesLen
-//
-//Same effect as strlen (length until \0)
-//See tests for more examples.
-//
-//Params
-//	bytes: bytes
-//
-//Return
-//  int: length of the fixed-bytes
-func FixedBytesLen(bytes []byte) int {
-	for idx, c := range bytes {
-		if c == 0 {
-			return idx
-		}
-	}
-	return len(bytes)
-}
-
-//FixedBytesToString
-//
-//Only the bytes until \0 when converting to string.
-//See tests for more examples.
-//
-//Params
-//	bytes: bytes
-//
-//Return
-//	string: string
-func FixedBytesToString(bytes []byte) string {
-	len := FixedBytesLen(bytes)
-	return string(bytes[:len])
-}
