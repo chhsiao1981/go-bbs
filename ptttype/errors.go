dddcb2794cad4d0d44153d3b36569eb6a9af796f
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2020-11-26 13:01:31 -0500

    user-id-already-exists

--- ptttype/errors.go
+++ ptttype/errors.go
@@ -3,6 +3,7 @@ package ptttype
 import "errors"
 
 var (
-	ErrInvalidUserID   = errors.New("invalid user id")
-	ErrInvalidFilename = errors.New("invalid filename")
+	ErrUserIDAlreadyExists = errors.New("user id already exists")
+	ErrInvalidUserID       = errors.New("invalid user id")
+	ErrInvalidFilename     = errors.New("invalid filename")
 )
